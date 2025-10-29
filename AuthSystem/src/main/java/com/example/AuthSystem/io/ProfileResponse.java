51a2c2bf458b70380f1133ac68b6688c2a320699
Nikhil Singh <nikhilsingh93p@gmail.com>
2025-10-30 00:09:21 +0530

    Phase1

--- AuthSystem/src/main/java/com/example/AuthSystem/io/ProfileResponse.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/io/ProfileResponse.java
@@ -1,4 +1,19 @@
 package com.example.AuthSystem.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProfileResponse {
+
+    private String userId;
+    private String name;
+    private String email;
+    private Boolean isAccountVerified;
+
 }
---
+++ AuthSystem/src/main/java/com/example/AuthSystem/service/ProfileServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.AuthSystem.service;
+
+import com.example.AuthSystem.Repo.UserRepository;
+import com.example.AuthSystem.io.ProfileRequest;
+import com.example.AuthSystem.io.ProfileResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileServiceImpl implements ProfileService{
+
+private final UserRepository userRepository;
+    @Override
+    public ProfileResponse createProfile(ProfileRequest request) {
+        return null;
+    }
+}
