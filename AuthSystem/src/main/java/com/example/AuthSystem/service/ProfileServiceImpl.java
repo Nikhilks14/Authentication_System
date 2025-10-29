8e8cd88576421184d0b182869e50db115c46b79e
Nikhil Singh <nikhilsingh93p@gmail.com>
2025-10-30 00:22:06 +0530

    Phase1

--- AuthSystem/src/main/java/com/example/AuthSystem/service/ProfileServiceImpl.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/service/ProfileServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.AuthSystem.service;
 
 import com.example.AuthSystem.Repo.UserRepository;
+import com.example.AuthSystem.entity.UserEntity;
 import com.example.AuthSystem.io.ProfileRequest;
 import com.example.AuthSystem.io.ProfileResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,8 @@ public class ProfileServiceImpl implements ProfileService{
 private final UserRepository userRepository;
     @Override
     public ProfileResponse createProfile(ProfileRequest request) {
+        UserEntity newProfile = converToUserEntity(request);
+        newProfile = userRepository.save(newProfile);
         return null;
     }
 }
